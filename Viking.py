81ce1065134f3c8d76810a4a09bde5667c25c64e
Winry <winry.tian@gmail.com>
2021-09-08 23:26:06 -0700

    Add separate functions for each action animation

--- Viking.py
+++ Viking.py
@@ -4,10 +4,12 @@
 class Viking:
     def __init__(self, x, y, sheet):
         self.pos = PVector(x, y)
-        self.vel = PVector()#.random2D() # same as PVector(0, 0) or PVector(0, 0, 0)
+        self.vel = PVector() # same as PVector(0, 0) or PVector(0, 0, 0)
         self.acc = PVector()
         self.sheet = sheet # a sheet of the class's animation.
-        self.frame = 0
+        self.index = 0
+        # keeps track of what part of the sheet I want to animate
+        self.y_pos = 0
     
     
     # applies a force to a particle using Newton's Second Law of Physics
@@ -23,13 +25,44 @@ class Viking:
         self.acc.mult(0) # same as resetting it to PVector()
     
     
-    # animates the Viking so that it appears to be executing an action.
-    def animate(self):
-        self.frame = frameCount//7 % 7
+    # animates the Viking so that it appears to be idling.
+    def animate_idle(self):
+        self.index = frameCount//10 % 7
+        self.y_pos = 0
+    
+    
+    # animates the Viking so that it appears to be running.
+    def animate_running(self):
+        self.index = frameCount//8 % 5
+        self.y_pos = 1
+    
+    
+    # animates the Viking so that it appears to be in midair, much like it should be here.
+    def animate_falling(self):
+        self.index = frameCount//12 % 4
+        self.y_pos = 2
+    
+    
+    # animates the Viking so that it appears to be swinging and jabbing its sword.
+    def animate_attack(self):
+        self.index = frameCount//8 % 9
+        self.y_pos = 3
+    
+    
+    # animates the Viking so that it appears to be jabbing its weapon and blocking an attack.
+    def animate_block(self):
+        self.index = frameCount//10 % 9
+        self.y_pos = 4
+    
+    
+    # animates the Viking so that it appears to have taken a fatal blow.
+    def animate_death(self):
+        self.index = frameCount//15 % 9
+        self.y_pos = 5
     
     
     # shows a Viking. Can be animated, mirrored, or rotated.
     def show(self):
-        sprite_frame = self.sheet.get(32 * self.frame, 0, 32, 32)
+        sprite_frame = self.sheet.get(32 * self.index, self.y_pos * 32, 32, 32)
         sprite_frame.resize(96, 96)
         image(sprite_frame, self.pos.x, self.pos.y)
--- sprites.pyde
+++ sprites.pyde
@@ -41,7 +41,12 @@ def draw():
     background(220, 79, 35)
     
     for viking in vikings:
-        viking.animate()
+        # viking.animate_idle()
+        # viking.animate_running()
+        # viking.animate_falling()
+        viking.animate_attack()
+        # viking.animate_block()
+        # viking.animate_death()
         viking.show()
         # viking.apply_force(PVector(0, 0.1))
         viking.update()
